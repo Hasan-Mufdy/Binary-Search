42d6f431daa4dc03ad185a1ede790ca851ce8a60
HasanMufdy <96997047+HasanMufdy@users.noreply.github.com>
2023-06-20 21:12:56 +0300

    Add project files.

---
+++ Binary-Search/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 4, 5, 6 };
+        Console.WriteLine(BinarySearch(nums, 2));
+    }
+
+    public static int BinarySearch(int[] arr, int key)
+    {
+        int low = 0;
+        int high = arr.Length - 1;
+
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] == key)
+            {
+                return mid;
+            }
+            else if (arr[mid] < key)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+
+}
